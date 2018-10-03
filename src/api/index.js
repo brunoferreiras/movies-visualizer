8ffa7b72992aca703413999dbb8d2ea0553ad829
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-10-03 20:57:38 -0300

    Create generic instance axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
 import { API_KEY, LANGUAGE, URL_MOVIE_DETAILS, URL_POPULAR_MOVIES, URL_SEARCH_MOVIE } from '../utilities/constants';
+import request from './request';
 
 export const getPopularMovies = page => {
-  return axios.get(`${URL_POPULAR_MOVIES}&page=${page}`);
+  return request.get(`${URL_POPULAR_MOVIES}&page=${page}`);
 };
 
 export const getMovieDetails = id => {
   const url = `${URL_MOVIE_DETAILS}${id}?api_key=${API_KEY}&language=${LANGUAGE}`;
-  return axios.get(url);
+  return request.get(url);
 };
 
 export const searchMovie = word => {
   const url = `${URL_SEARCH_MOVIE}${word}`;
-  return axios.get(url);
+  return request.get(url);
 };
---
+++ src/api/request.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+const request = axios.create({
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
+});
+
+request.interceptors.request.use(
+  function(config) {
+    // Faz algo antes de requisição ser enviada
+    console.log('URL REQUEST AXIOS: ', config.url);
+
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
+
+request.interceptors.response.use(
+  function(response) {
+    console.log('RESPONSE AXIOS: ', response);
+    return response;
+  },
+  function(error) {
+    console.log('RESPONSE ERROR AXIOS: ', error);
+    if (error.response) {
+      // Quando a requisição é feita e o servidor responde com um status diferente de 200.
+      return Promise.reject(new Error(error.response.data.error));
+    } else if (error.request) {
+      // Quando a requisição é feita, porém não é recebido nenhuma resposta.
+      return Promise.reject(
+        new Error(
+          'Não foi possível se conectar com o servidor! Por favor, tente novamente mais tarde.'
+        )
+      );
+    } else {
+      // Algo aconteceu na configuração da solicitação que acionou o erro.
+      console.log('Error', error.message);
+      return Promise.reject(
+        new Error(
+          'Algo inesperado aconteceu! Por favor, tente novamente mais tarde.'
+        )
+      );
+    }
+  }
+);
+
+export default request;
