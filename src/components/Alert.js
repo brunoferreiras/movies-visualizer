d0921019b2f37ef48947b234297ec64652bb8236
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-10-03 21:54:41 -0300

    Create component alert

---
+++ src/components/Alert.js
@@ -0,0 +1,57 @@
+import Snackbar from '@material-ui/core/Snackbar';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import React from 'react';
+import SnackbarContentWrapperUI from './SnackbarContentWrapperUI';
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit
+  }
+});
+
+class Alert extends React.Component {
+  state = {
+    open: true
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { variant, message } = this.props;
+
+    return (
+      <div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center'
+          }}
+          open={this.state.open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+        >
+          <SnackbarContentWrapperUI
+            onClose={this.handleClose}
+            variant={variant}
+            message={message}
+          />
+        </Snackbar>
+      </div>
+    );
+  }
+}
+
+Alert.propTypes = {
+  classes: PropTypes.object.isRequired,
+  variant: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired
+};
+
+export default withStyles(styles)(Alert);
